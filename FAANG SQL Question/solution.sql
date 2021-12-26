de65b864d743db9a8abd76ae74496b8d1f2157d2
Rushikesh <rushikeshpawar197@gmail.com>
2021-12-26 23:46:21 +0100

    commit

---
+++ FAANG SQL Question/solution.sql
@@ -0,0 +1,52 @@
+use faang;
+
+select * from warehouse;
+
+
+
+WITH WH as
+		(select * from warehouse order by event_datetime desc),
+	days as
+		(select event_datetime, onhandquantity
+		 	  , date_sub(event_datetime, interval 90 day) as day90
+		 	  , date_sub(event_datetime, interval 180 day) as day180
+		 	  , date_sub(event_datetime, interval 270 day) as day270
+		 	  , date_sub(event_datetime, interval 365 day) as day365
+		 from WH limit 1),
+         
+	inv_90_days as 
+		(select sum(OnHandQuantityDelta) as daysold_90
+        from WH cross join days d
+        where event_type = 'InBound'
+        and WH.event_datetime >= d.day90),
+        
+	inv_90_days_final as 
+		(select case when daysold_90 > d.onhandquantity then d.onhandquantity
+        else daysold_90
+        end as daysold_90
+        from inv_90_days
+        cross join days d),
+        
+    inv_180_days as
+		(select sum(OnHandQuantityDelta) as daysold_180
+        from WH cross join days d
+        where event_type = 'Inbound'
+        and WH.event_datetime >= d.day180),
+        
+	inv_180_days_final as 
+		(select 
+        case when daysold_180 > d.OnHandQuantity then d.OnHandQuantity
+        else daysold_180
+        end as daysold_180
+        from inv_180_days
+        cross join days d)
+        
+
+select * from inv_180_days_final;
+
+
+
+
+
+
+         
