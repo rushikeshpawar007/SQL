75aa84bead467aa21ef74bc5a91e3bfe800d83fc
Rushikesh <rushikeshpawar197@gmail.com>
2021-12-30 17:41:20 +0100

    update

--- FAANG SQL Question/solution.sql
+++ FAANG SQL Question/solution.sql
@@ -39,10 +39,39 @@ WITH WH as
         else daysold_180
         end as daysold_180
         from inv_180_days
-        cross join days d)
+        cross join days d),
         
+inv_270_days as
+		(select sum(OnHandQuantityDelta) as daysold_270
+        from WH cross join days d
+        where event_type = 'Inbound'
+        and WH.event_datetime >= d.day270),
+        
+inv_270_days_final as
+		( select
+        case when daysold_270 > d.OnhandQuantity then d.OnhandQuantity
+        else daysold_270
+        end as daysold_270
+        from inv_270_days
+        cross join days d),
+        
+        
+inv_365_days as
+		(select sum(OnHandQuantityDelta) as daysold_365
+        from WH cross join days d
+        where event_type = 'Inbound'
+        and WH.event_datetime >= d.day365),
+        
+inv_365_days_final as 
+		(select 
+        case when daysold_365 > d.OnHandQuantity then OnHandQuantity
+        else daysold_365
+        end as daysold_365
+        from inv_365_days
+        cross join days d)
 
-select * from inv_180_days_final;
+
+select * from inv_365_days_final;
 
 
 
